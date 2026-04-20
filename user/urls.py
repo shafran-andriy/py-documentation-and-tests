e637facfb55c65bdaddcdfee08886f4c007139d3
Andriy Shafran <shafran.andriy@gmail.com>
2026-04-20 18:47:06 +0300

    Added all neccesery functionality and settings for Swagger and Redoc

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from user.views import CreateUserView, CreateTokenView, ManageUserView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 app_name = "user"
 
@@ -7,4 +12,13 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
 ]
