4794e1934f6e44b44dd7b715687226ac6baf56f1
Andriy Shafran <shafran.andriy@gmail.com>
2026-04-20 18:58:16 +0300

    Added SIMPLE_JWT to settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -161,4 +161,10 @@ SPECTACULAR_SETTINGS = {
         "defaultModelExpandDepth": 2,
     },
     # OTHER SETTINGS
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
 }
